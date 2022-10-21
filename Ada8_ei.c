b94fb6a1b43481036a08c162fc0199fe4418a42d
ManiLucas <57599828+ManiLucas@users.noreply.github.com>
2022-10-21 14:06:51 -0500

    Create Ada8_ei.c

---
+++ Ada8_ei.c
@@ -0,0 +1,23 @@
+/* Programa: muestar un mensaje o frase cada x segundos*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+//arreglo confrases
+
+int main( int argc, char* argv[] )
+{
+    int SLEEPTIME = 10;
+   	int cont=0;
+
+   for( ; ; ) {
+        fprintf(stderr,"%s", "\007");
+		cont++;
+		fprintf(stderr,"Monitoreando el correo de linux %d\n",cont);
+
+      }  
+      sleep(SLEEPTIME);
+   }
+  return 0; 
