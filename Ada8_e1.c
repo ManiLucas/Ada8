bd24d109083bd335aebce021b70628de3a9ac8c6
Manuel Lucas <Manuel.Lucas.S@hotmail.com>
2022-10-29 00:29:12 -0500

    arreglo de frases añadida

--- Ada8_e1.c
+++ Ada8_e1.c
@@ -6,22 +6,37 @@
 #include <unistd.h>
 
 //arreglo con frases
-struct frases{
-    char frase[100];
-};
 
 int main( int argc, char* argv[] )
 {
-    int SLEEPTIME = 10;
+    char *palabra[] = {"Recuerda que si puedes imaginarlo, puedes crearlo", 
+                       "Lo unico imposible es aquello que no intentas",
+                       "Cuando miras al abismo el abismo te regresa la mirada",
+                       "La peor lucha es la que no se hace ",
+                       "No hay noche, por larga que sea, que no encuentre el día ",
+                       "Aquellos que no quieren imitar nada no producirán nada",
+                       "Nunca moriría por mis creencias, porque podría estar equivocado",
+                       "No os toméis la vida demasiado en serio; de todas maneras, no saldréis vivos de esta ",
+                       };
+    
+    int SLEEPTIME = 5;
    	int cont=0;
+    int i=0, tam;
+    tam = sizeof(palabra)/sizeof(char *);
 
    for( ; ; ) {
         fprintf(stderr,"%s", "\007");
 		cont++;
-		fprintf(stderr,"Monitoreando el correo de linux %d\n",cont);
-
-      }  
-      sleep(SLEEPTIME);
+		fprintf(stderr,"%s\n", palabra[i]);
+        //printf("%d\n",i);
+        i++;
+        if(i==tam){
+            i = tam%i;
+            //printf("fin\n");
+        }
+        sleep(SLEEPTIME);
+    }  
+      
    
   return 0; 
 }
